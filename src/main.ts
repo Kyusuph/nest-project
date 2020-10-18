9b85417c4826c5d25954fe20cce2c59270588783
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-10-18 10:36:56 +0300

    Adding logger custom logics

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {logger: ['error', 'debug']});
   const port = 3000;
   await app.listen(port);
   logger.log(`Application started and listening at port ${port}`);
